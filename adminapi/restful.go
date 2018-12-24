e836195826370da220af19a068b88b9054322540
tian <xiaoliang.tian@gmail.com>
2018-12-24 16:19:48 +0800

    upgrade go chassis, remove admin API of fetching route  rules

--- adminapi/restful.go
+++ adminapi/restful.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/go-chassis/go-chassis/core/common"
-	"github.com/go-chassis/go-chassis/core/config/model"
 	"github.com/go-chassis/go-chassis/core/router"
 	"github.com/go-chassis/go-chassis/pkg/metrics"
 	"github.com/go-chassis/go-chassis/server/restful"
@@ -28,14 +27,6 @@ func (a *Admin) GetMetrics(context *restful.Context) {
 	resp := context.ReadResponseWriter()
 	req := context.ReadRequest()
 	promhttp.HandlerFor(metrics.GetSystemPrometheusRegistry(), promhttp.HandlerOpts{}).ServeHTTP(resp, req)
-}
-
-//RouteRule returns all router configs
-func (a *Admin) RouteRule(context *restful.Context) {
-	routerConfig := &model.RouterConfig{
-		Destinations: router.DefaultRouter.FetchRouteRule(),
-	}
-	context.WriteHeaderAndJSON(http.StatusOK, routerConfig, "text/vnd.yaml")
 }
 
 //RouteRuleByService returns route config for particular service
@@ -65,7 +56,6 @@ func (a *Admin) URLPatterns() []restful.Route {
 	return []restful.Route{
 		{Method: http.MethodGet, Path: "/v1/mesher/version", ResourceFuncName: "GetVersion"},
 		{Method: http.MethodGet, Path: "/v1/mesher/metrics", ResourceFuncName: "GetMetrics"},
-		{Method: http.MethodGet, Path: "/v1/mesher/routeRule", ResourceFuncName: "RouteRule"},
 		{Method: http.MethodGet, Path: "/v1/mesher/routeRule/{serviceName}", ResourceFuncName: "RouteRuleByService"},
 		{Method: http.MethodGet, Path: "/v1/mesher/health", ResourceFuncName: "MesherHealth"},
 	}
