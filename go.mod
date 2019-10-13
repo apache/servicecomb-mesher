ea5dd683f154b8b5680753ec8c0e0fdb63287951
surechen <814464284@qq.com>
2019-10-13 17:46:29 +0800

    Support for skywalking. for jira issue SCB-1497
    Signed-off-by: surechen <chenshuo17@huawei.com>

--- go.mod
+++ go.mod
@@ -9,30 +9,29 @@ require (
 	github.com/go-chassis/go-archaius v0.20.0
 	github.com/go-chassis/go-chassis v1.7.2-0.20190917011915-d49fa9cdd27d
 	github.com/go-chassis/go-chassis-config v0.10.0
+	github.com/go-chassis/go-chassis-plugins v0.0.0-20190507123504-83783d9c730e // indirect
 	github.com/go-chassis/gohessian v0.0.0-20180702061429-e5130c25af55
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-mesh/openlogging v1.0.1-0.20181205082104-3d418c478b2d
 	github.com/gogo/googleapis v1.1.0 // indirect
 	github.com/gogo/protobuf v1.2.0
-	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
-	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/lyft/protoc-gen-validate v0.0.11 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/pierrec/lz4 v2.3.0+incompatible // indirect
 	github.com/prometheus/client_golang v0.9.1
 	github.com/prometheus/client_model v0.0.0-20190115171406-56726106282f
 	github.com/prometheus/procfs v0.0.0-20190117184657-bf6a532e95b1 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563 // indirect
 	github.com/smartystreets/assertions v0.0.0-20190116191733-b6c0e53d7304 // indirect
-	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/stretchr/testify v1.3.0
+	github.com/tetratelabs/go2sky v0.1.1-0.20190703154722-1eaab8035277
 	github.com/uber-go/atomic v1.3.2 // indirect
 	github.com/urfave/cli v1.20.1-0.20181029213200-b67dcf995b6a
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
-	golang.org/x/time v0.0.0-20180412165947-fbb02b2291d2 // indirect
-	google.golang.org/grpc v1.16.0
-	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.2.1
-	k8s.io/apimachinery v0.0.0-20181022183627-f71dbbc36e12
+	google.golang.org/grpc v1.19.1
+	gopkg.in/yaml.v2 v2.2.2
+	k8s.io/apimachinery v0.0.0-20181108045954-261df694e725
 	k8s.io/client-go v9.0.0+incompatible
 )
 
@@ -40,13 +39,13 @@ replace (
 	cloud.google.com/go v0.28.0 => github.com/GoogleCloudPlatform/google-cloud-go v0.28.0
 	github.com/kubernetes/client-go => ../k8s.io/client-go
 	github.com/openzipkin-contrib/zipkin-go-opentracing v0.3.5 => github.com/go-chassis/zipkin-go-opentracing v0.3.5-0.20190321072447-42cf74fc2a92
+	github.com/tetratelabs/go2sky v0.1.1-0.20190703154722-1eaab8035277 => github.com/SkyAPM/go2sky v0.1.1-0.20190703154722-1eaab8035277
 	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 => github.com/golang/crypto v0.0.0-20181030102418-4d3f4d9ffa16
 	golang.org/x/net v0.0.0-20180906233101-161cd47e91fd => github.com/golang/net v0.0.0-20180906233101-161cd47e91fd
 	golang.org/x/oauth2 v0.0.0-20180207181906-543e37812f10 => github.com/golang/oauth2 v0.0.0-20180207181906-543e37812f10
 	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f => github.com/golang/sync v0.0.0-20180314180146-1d60e4601c6f
 	golang.org/x/sys v0.0.0-20180909124046-d0be0721c37e => github.com/golang/sys v0.0.0-20180909124046-d0be0721c37e
 	golang.org/x/sys v0.0.0-20181031143558-9b800f95dbbc => github.com/golang/sys v0.0.0-20181031143558-9b800f95dbbc
-
 	golang.org/x/text v0.3.0 => github.com/golang/text v0.3.0
 	golang.org/x/time v0.0.0-20180412165947-fbb02b2291d2 => github.com/golang/time v0.0.0-20180412165947-fbb02b2291d2
 	google.golang.org/appengine v1.2.0 => github.com/golang/appengine v1.2.0
---
+++ proxy/handler/skywalking_handler.go
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package handler
+
+import (
+	"github.com/apache/servicecomb-mesher/proxy/pkg/skywalking"
+	"github.com/go-chassis/go-chassis/core/common"
+	"github.com/go-chassis/go-chassis/core/handler"
+	"github.com/go-chassis/go-chassis/core/invocation"
+	"github.com/go-mesh/openlogging"
+	"github.com/tetratelabs/go2sky"
+	skycom "github.com/tetratelabs/go2sky/reporter/grpc/common"
+	"strconv"
+)
+
+const (
+	HTTPPrefix = "http://"
+)
+
+const (
+	HTTPClientComponentID = 2
+	HTTPServerComponentID = 49
+)
+
+//SkyWalkingProviderHandler struct
+type SkyWalkingProviderHandler struct {
+}
+
+//Handle is for provider
+func (sp *SkyWalkingProviderHandler) Handle(chain *handler.Chain, i *invocation.Invocation, cb invocation.ResponseCallBack) {
+	openlogging.GetLogger().Debugf("SkyWalkingProviderHandler begin. inv:%#v", *i)
+	span, _, err := skywalking.CreateEntrySpan(i)
+	if err != nil {
+		openlogging.GetLogger().Errorf("CreateEntrySpan error:%s", err.Error())
+	}
+	chain.Next(i, func(r *invocation.Response) (err error) {
+		err = cb(r)
+		switch i.Protocol {
+		case common.ProtocolRest:
+			span.Tag(go2sky.TagHTTPMethod, i.Protocol)
+			span.Tag(go2sky.TagURL, HTTPPrefix+i.MicroServiceName+i.URLPathFormat)
+			span.Tag(go2sky.TagStatusCode, strconv.Itoa(r.Status))
+			span.SetSpanLayer(skycom.SpanLayer_Http)
+			span.SetComponent(HTTPServerComponentID)
+		default:
+		}
+		span.End()
+		return
+	})
+}
+
+//Name return provider name
+func (sp *SkyWalkingProviderHandler) Name() string {
+	return skywalking.SkyWalkingProvider
+}
+
+//NewSkyWalkingProvier return provider handler for SkyWalking
+func NewSkyWalkingProvier() handler.Handler {
+	return &SkyWalkingProviderHandler{}
+}
+
+//SkyWalkingConsumerHandler struct
+type SkyWalkingConsumerHandler struct {
+}
+
+//Handle is for consumer
+func (sc *SkyWalkingConsumerHandler) Handle(chain *handler.Chain, i *invocation.Invocation, cb invocation.ResponseCallBack) {
+	openlogging.GetLogger().Debugf("SkyWalkingConsumerHandler begin:%#v", *i)
+	span, ctx, err := skywalking.CreateEntrySpan(i)
+	if err != nil {
+		openlogging.GetLogger().Errorf("CreateEntrySpan error:%s", err.Error())
+	}
+	spanExit, err := skywalking.CreateExitSpan(ctx, i)
+	if err != nil {
+		openlogging.GetLogger().Errorf("CreateExitSpan error:%s", err.Error())
+	}
+	chain.Next(i, func(r *invocation.Response) (err error) {
+		err = cb(r)
+		switch i.Protocol {
+		case common.ProtocolRest:
+			span.Tag(go2sky.TagHTTPMethod, i.Protocol)
+			span.Tag(go2sky.TagURL, i.Endpoint+i.URLPathFormat)
+			span.Tag(go2sky.TagStatusCode, strconv.Itoa(r.Status))
+			span.SetSpanLayer(skycom.SpanLayer_Http)
+			span.SetComponent(HTTPServerComponentID)
+
+			spanExit.Tag(go2sky.TagHTTPMethod, i.Protocol)
+			spanExit.Tag(go2sky.TagURL, i.Endpoint+i.URLPathFormat)
+			spanExit.Tag(go2sky.TagStatusCode, strconv.Itoa(r.Status))
+			spanExit.SetSpanLayer(skycom.SpanLayer_Http)
+			spanExit.SetComponent(HTTPClientComponentID)
+		default:
+		}
+		spanExit.End()
+		span.End()
+		openlogging.GetLogger().Debugf("SkyWalkingConsumerHandler end.")
+		return
+	})
+}
+
+//Name return consumer name
+func (sc *SkyWalkingConsumerHandler) Name() string {
+	return skywalking.SkyWalkingConsumer
+}
+
+//NewSkyWalkingConsumer return consumer handler for SkyWalking
+func NewSkyWalkingConsumer() handler.Handler {
+	return &SkyWalkingConsumerHandler{}
+}
+
+func init() {
+	handler.RegisterHandler(skywalking.SkyWalkingProvider, NewSkyWalkingProvier)
+	handler.RegisterHandler(skywalking.SkyWalkingConsumer, NewSkyWalkingConsumer)
+}
