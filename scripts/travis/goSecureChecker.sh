cbe072e7d62017024ce5ae4310bc95fab9658dc6
“t_xinlin@sina.com <Happy100>
2020-04-21 16:18:33 +0800

    Fix goSec

--- scripts/travis/goSecureChecker.sh
+++ scripts/travis/goSecureChecker.sh
@@ -18,7 +18,8 @@ gosec ./... > result.txt
 cat result.txt
 rm -rf result.txt
 issueCount=$(gosec ./... | grep "Issues" | awk -F":" '{print $2}' | sed 's/[[:space:]]//g')
-if [ $? == 0 ] && [ $issueCount -le 35 ] ; then
+echo "issueCount="$issueCount
+if [ $? == 0 ] && [[ $issueCount -le 35 ]] ; then
 	echo "No GoSecure warnings found"
 	exit 0
 else
