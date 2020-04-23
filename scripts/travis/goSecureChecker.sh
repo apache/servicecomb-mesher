0f3b10215dcd8cef2b44b4c07f61cf3a45e67e14
“t_xinlin@sina.com <Happy100>
2020-04-23 14:24:36 +0800

    Fix goSec shell error

--- scripts/travis/goSecureChecker.sh
+++ scripts/travis/goSecureChecker.sh
@@ -17,13 +17,12 @@
 gosec ./... > result.txt
 cat result.txt
 rm -rf result.txt
-issueCount=$(gosec ./... | grep "Issues" | awk -F":" '{print $2}' | sed 's/[[:space:]]//g')
+issueCount=$((gosec ./... | grep "Issues" | awk -F":" '{print $2}' | sed 's/[[:space:]]//g'))
 echo "issueCount="$issueCount
-if [ $? == 0 ]; then
-	if [ $issueCount -le 35 ]; then
-		echo "No GoSecure warnings found"
-		exit 0
-	fi
+if [ $? == 0 ] && [ $issueCount -le 35 ]; then
+	echo "No GoSecure warnings found"
+	exit 0
+else
+	echo "GoSecure Warnings found"
+	exit 1
 fi
-echo "GoSecure Warnings found"
-exit 1
