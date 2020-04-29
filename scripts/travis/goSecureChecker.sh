76b2ca54b4829295d419fb736c68e450add033c8
“t_xinlin@sina.com <Happy100>
2020-04-29 17:16:14 +0800

    fmt shell file

--- scripts/travis/goSecureChecker.sh
+++ scripts/travis/goSecureChecker.sh
@@ -16,7 +16,7 @@
 
 gosec ./... > result.txt
 cat result.txt
-issueCount=$(cat result.txt | grep "Issues" | sed 's/[[:space:]]//g' | awk -F":" '{printf "%d",$2}')
+issueCount=$(cat result.txt | grep "Issues" | awk -F":" '{print $2}')
 rm -rf result.txt
 if (($? == 0 && 35 > $issueCount)); then
 	echo "No GoSecure warnings found"
